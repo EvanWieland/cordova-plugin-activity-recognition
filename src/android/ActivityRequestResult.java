81babc92c19fbc0abf839be9c0259bf444b72087
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2016-10-08 15:37:33 +0530

    Reformat src/android/*.java files

--- src/android/ActivityRequestResult.java
+++ src/android/ActivityRequestResult.java
@@ -1,24 +1,21 @@
 package cordova.plugin.activity.recognition;
 
-import org.json.JSONObject;
 import org.json.JSONException;
+import org.json.JSONObject;
 
-public class ActivityRequestResult 
-{
-	public String ActivityType;
-	public int Propability;
-	
-	public ActivityRequestResult()
-	{
-		ActivityType = "NoActivityYet";
-		Propability = 0;
-	}
-	
-	public JSONObject GetJSONObject() throws JSONException 
-	{
-		JSONObject result = new JSONObject();
-		result.put("ActivityType", ActivityType);
-		result.put("Propability", Propability);
-		return result;
-	}
+public class ActivityRequestResult {
+  public String ActivityType;
+  public int Propability;
+
+  public ActivityRequestResult() {
+    ActivityType = "NoActivityYet";
+    Propability = 0;
+  }
+
+  public JSONObject GetJSONObject() throws JSONException {
+    JSONObject result = new JSONObject();
+    result.put("ActivityType", ActivityType);
+    result.put("Propability", Propability);
+    return result;
+  }
 }
